06162325d4003d5a447a7b3904cb165c1825b07a
Kithmina Siriwardana <it21020230@my.sliit.lk>
2024-10-21 23:42:24 +0530

    favicon changed

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,22 +1,27 @@
-import "../app/assets/css/tailwind.css"
-import '../app/assets/css/materialdesignicons.min.css';
-import { Poppins } from 'next/font/google'
+import "../app/assets/css/tailwind.css";
+import "../app/assets/css/materialdesignicons.min.css";
+import { Poppins } from "next/font/google";
 
-const poppins = Poppins({ 
-  subsets: ['latin'],
-  variable: '--font-poppins',
-  weight: ['400','500','600', '700'],
- })
+const poppins = Poppins({
+  subsets: ["latin"],
+  variable: "--font-poppins",
+  weight: ["400", "500", "600", "700"],
+});
 
 export const metadata = {
-  title: 'Dennis - Next Js Personal Portfolio Template',
-  description: 'Dennis - Next Js Personal Portfolio Template',
-}
+  title: "Optical Insight",
+  description:
+    "Optical Insight - Enhancing Ophthalmic Diagnostics with Automated Detection of Retinal Abnormalities",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${poppins.variable} font-poppins text-base text-black dark:text-white dark:bg-slate-900 `}>{children}</body>
+      <body
+        className={`${poppins.variable} font-poppins text-base text-black dark:text-white dark:bg-slate-900 `}
+      >
+        {children}
+      </body>
     </html>
-  )
+  );
 }
